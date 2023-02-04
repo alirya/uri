00959fb416004109ae7998613bdd7ab5d1a00bb0
dikac <rigel234@yahoo.com>
2023-02-04 15:35:28 +0700

    ref: implement split validator

--- src/user-info/validator/user-info.ts
+++ src/user-info/validator/user-info.ts
@@ -4,21 +4,21 @@ import {UserInfoClassParameters} from '../validatable/user-info';
 import SimpleValidator from '@alirya/validator/simple';
 import ReadonlyValidatable from '../validatable/readonly';
 
-export function UserInfoParameters() : SimpleValidator<string, string, ReadonlyValidatable<string>>;
+export function UserInfoParameters() : SimpleValidator<string, string, string>;
 
 export function UserInfoParameters<MessageType>(
     message : ValidatableParameters<string, MessageType>
-) : SimpleValidator<string, string, ReadonlyValidatable<MessageType>>;
+) : SimpleValidator<string, string, MessageType>;
 
 export function UserInfoParameters<MessageType>(
     message : ValidatableParameters<string, MessageType|string> = UserInfoMessage.Parameters
-) : SimpleValidator<string, string, ReadonlyValidatable<MessageType|string>> {
+) : SimpleValidator<string, string, MessageType|string> {
 
     return function (value) {
 
         return new UserInfoClassParameters(value, message);
 
-    } as SimpleValidator<string, string, ReadonlyValidatable<MessageType|string>>;
+    } as SimpleValidator<string, string, MessageType|string>;
 }
 
 
@@ -26,19 +26,19 @@ export interface UserInfoParameter<MessageType> {
     message ?: ValidatableParameter<string, MessageType|string>;
 }
 
-export function UserInfoParameter({}) : SimpleValidator<string, string, ReadonlyValidatable<string>>;
+export function UserInfoParameter({}) : SimpleValidator<string, string, string>;
 
 export function UserInfoParameter<MessageType>(
     {
         message,
     } : Required<UserInfoParameter<MessageType>>
-) : SimpleValidator<string, string, ReadonlyValidatable<MessageType>>;
+) : SimpleValidator<string, string, MessageType>;
 
 export function UserInfoParameter<MessageType>(
     {
         message = UserInfoMessage.Parameter,
     } : Required<UserInfoParameter<MessageType|string>>
-) : SimpleValidator<string, string, ReadonlyValidatable<MessageType|string>> {
+) : SimpleValidator<string, string, MessageType|string> {
 
     return UserInfoParameters((value, valid) => message({value, valid}));
 }
